33709b24db74bc782aa0bcecf01b9549fff793db
Pranav Dhulipala <76590160+pranavdhulipala0@users.noreply.github.com>
2024-03-19 16:47:25 +0530

    Fixed bug

--- pv6.js
+++ pv6.js
@@ -12,7 +12,7 @@ const eventLogger = {
 
     sendEventData: (eventType, event) => {
         try{
-        var timing = window.performance;
+        var timing = window.performance.timing;
         axios.post('http://localhost:5000/api/middleware/getDetails', {
             tenantId:user_details.tenantId,
             userId:user_details.userId,
