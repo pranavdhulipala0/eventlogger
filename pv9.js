36db8f97c864e6e9a8253c0e2138bf65227bce17
Pranav Dhulipala <76590160+pranavdhulipala0@users.noreply.github.com>
2024-03-19 17:07:49 +0530

    Update and rename pv8.js to pv9.js

--- pv9.js
+++ pv9.js
@@ -11,21 +11,15 @@ const eventLogger = {
     },
 
     sendEventData: (eventType, event) => {
-        try{
-        var timing = window.performance.timing;
         axios.post('http://localhost:5000/api/middleware/getDetails', {
             tenantId:user_details.tenantId,
             userId:user_details.userId,
             platform: navigator.platform,
             eventType: eventType,
-            loadTime: timing.loadEventEnd - timing.navigationStart,
             eventLocation: event.currentTarget.location.href,
             xCoord: event.clientX,
             yCoord: event.clientY,
         });
-    }
-    catch(e){
-        console.log("Couldn't send");
     }
 };
 
