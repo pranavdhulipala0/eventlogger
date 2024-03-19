48a02de6753c68e3117b41eb576a8b37c2ede2ad
Pranav Dhulipala <76590160+pranavdhulipala0@users.noreply.github.com>
2024-03-19 17:03:04 +0530

    Update pv8.js

--- pv7.js
+++ pv7.js
@@ -11,22 +11,22 @@ const eventLogger = {
     },
 
     sendEventData: (eventType, event) => {
-        // try{
-        // var timing = window.performance.timing;
+        try{
+        var timing = window.performance.timing;
         axios.post('http://localhost:5000/api/middleware/getDetails', {
             tenantId:user_details.tenantId,
             userId:user_details.userId,
             platform: navigator.platform,
             eventType: eventType,
-            // loadTime: timing.loadEventEnd - timing.navigationStart;
+            loadTime: timing.loadEventEnd - timing.navigationStart,
             eventLocation: event.currentTarget.location.href,
             xCoord: event.clientX,
             yCoord: event.clientY,
         });
-    // }
-    // catch(e){
-    //     console.log("Couldn't send");
-    // }
+    }
+    catch(e){
+        console.log("Couldn't send");
+    }
 };
 
 eventLogger.init();
