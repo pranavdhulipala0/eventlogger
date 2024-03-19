49062ec8bdcdc57c868b91a699932cc4e7754a96
Pranav Dhulipala <76590160+pranavdhulipala0@users.noreply.github.com>
2024-03-19 16:26:17 +0530

    Added Load time testing

--- pv4.js
+++ pv4.js
@@ -11,11 +11,13 @@ const eventLogger = {
     },
 
     sendEventData: (eventType, event) => {
+        var timing = window.performance;
         axios.post('http://localhost:5000/api/middleware/getDetails', {
             tenantId:user_details.tenantId,
             userId:user_details.userId,
             platform: navigator.platform,
             eventType: eventType,
+            loadTime: timing.loadEventEnd - timing.navigationStart;
             eventLocation: event.currentTarget.location.href,
             xCoord: event.clientX,
             yCoord: event.clientY,
